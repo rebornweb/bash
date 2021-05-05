57423169be203787d4cc02ad3a183ff722fed9a9
Cloud User <masterchief@halo-ring.ii0e5ltbvsvu1frs3cmdwiw32d.dx.internal.cloudapp.net>
2021-05-05 00:38:32 +0000

    Python Name and Age Solution

--- python/charinput.py
+++ python/charinput.py
@@ -1,7 +1,20 @@
 #Python
+import datetime
 
-
-fullName = input ("Type your Fullname please: \n ")
+fullName = input("Type your Fullname please: \n ")
 input("Fullname: " + fullName + " is entered \n Press [ENTER] to Continue \n ")
-print (fullName + " You will turn 100 tomorrow Happy Birthday! You will receive a food voucher ")
+age = input("Please enter your age: ")
+
+input("You are " + age + " years young")
+
+year = datetime.datetime.today().year
+
+ageInt = int(age)
+
+centInt = int((100 - ageInt)  + year)
+cent = str(centInt)
+
+print("You will be 100 years young at year " + cent)
+
+#print (fullName + " You will turn 100 tomorrow Happy Birthday! You will receive a food voucher ")
 
