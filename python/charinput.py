79f73718ae2ee28a2f1082d360a190183cabdf43
Cloud User <masterchief@halo-ring.ii0e5ltbvsvu1frs3cmdwiw32d.dx.internal.cloudapp.net>
2021-05-05 00:13:59 +0000

    Python coding

---
+++ python/charinput.py
@@ -0,0 +1,7 @@
+#Python
+
+
+fullName = input ("Type your Fullname please: \n ")
+input("Fullname: " + fullName + " is entered \n Press [ENTER] to Continue \n ")
+print (fullName + " You will turn 100 tomorrow Happy Birthday! You will receive a food voucher ")
+
