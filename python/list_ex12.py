f57cb397703410ab5d2d8d2b23cc47bcdac0a004
Cloud User <masterchief@halo-ring.ii0e5ltbvsvu1frs3cmdwiw32d.dx.internal.cloudapp.net>
2021-05-07 03:59:24 +0000

    update python

---
+++ python/list_ex12.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def firstL ():
+    listA =  [1,2,3,4,5]
+    f = listA[0]
+    l = listA[-1]
+    print("This is the first obj in the array " + str(f) + "\n")
+    print ("Last obj in the list or array " + str(l))
+
+firstL()
