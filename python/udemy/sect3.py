6ae10fccaa3b509bf5edda742f74b1fb0f2a9053
rebornweb <andrei.nicolas@hotmail.com>
2021-09-03 11:59:45 +1200

    Udemy Update

---
+++ python/udemy/sect3.py
@@ -0,0 +1,12 @@
+
+single = "She sells " + "Seashells by the Seashore!'"
+length_1 = ( len(single) );
+print ( single.upper() + '\nLength of string : ' )
+print( length_1) 
+print( ' :) ' * 3)
+print( 'The version for Python is {}'.format(3) )
+print('####')
+
+#Table 
+print( '{0:8} | {1:<8}'.format( 'Fruit','Quantity' ))
+print( '{0:8} | {1:<8}'.format( 'Apples', 3 ))
